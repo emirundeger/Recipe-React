8e73be48aed1cf6968844eea0b694deeccc29f46
eundeger <emirundeger@gmail.com>
2019-11-16 15:29:01 +0000

    Changes made for getting the previous results after pressing to the go home button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import Form from "./components/Form";
 import Recipes from "./components/Recipes";
 
-const API_KEY = "d3ab033003c2e546e131f5b45402e3e9";
+//const API_KEY = "d3ab033003c2e546e131f5b45402e3e9";
+const API_KEY = "4a4476650d4e1b3923fbd1ae5cbd3ec6";
 
 class App extends Component {
   state = {
@@ -17,6 +18,15 @@ class App extends Component {
     );
     const data = await api_call.json();
     this.setState({ recipes: data.recipes });
+  };
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes: recipes });
+  };
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
   };
   render() {
     return (
